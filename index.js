eb039910c54db9ad8231141a561defd2b575f0d7
Daniel <dwiest4@gmail.com>
2023-06-07 09:53:14 -0700

    Updated imports, added: body-parser, mongoose, mongoose.schema, assert.
    Set up connection to mongoose.
    
    Updated Basic Configuration to use bodyParser
    
     Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose ');
+const Schema = mongoose.Schema;
+const assert = require('assert');
 
+//connect to server
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+  .then(connection => {
+    console.log('Connected to MongoDB DB')
+  })
+  .catch(error => {
+    console.log(error.message)
+  });
+
+//Basic Configuration
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
